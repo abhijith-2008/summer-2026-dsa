1e81205290834fd7b5523ae50d0f5801f55efab3
Abhijith Sarma <abhijithchandavaram@gmail.com>
2026-06-09 13:11:25 +0530

    Added largest element program

---
+++ Arrays/LargestNumber.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {1, 4, 3, 6, 5};
+    int n = 5;
+
+    int max = arr[0];
+
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    printf("Largest element = %d\n", max);
+
+    return 0;
+}
