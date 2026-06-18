8d35a92ab69336b34b886c4c69197141cfe047d4
Abhijith Sarma <abhijithchandavaram@gmail.com>
2026-06-18 13:39:59 +0530

    Insertion Sort

---
+++ Arrays/InsertionSort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for(int i=0; i<n; i++)
+    {
+        printf("Enter element %d: ", i+1);
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i=1; i<n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while(j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+
+    printf("\nSorted array:\n");
+
+    for(int i=0; i<n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
